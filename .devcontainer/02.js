fc23b055dfaae6a435ea80e5bb9b83d83fea92e3
Vasudevsuthar <133574340+Vasudevsuthar@users.noreply.github.com>
2024-04-04 07:22:43 +0000

    variables

---
+++ .devcontainer/02.js
@@ -0,0 +1,16 @@
+// Variables //
+
+const userId = 213213;
+let userName = "Vasu Dev";
+var userEmail = "vasu@gmail.com";
+userAdd = "Rajasthan";
+let accountState;
+
+// userId = 1; Const value cannot be changed.
+
+userName = "Vasu";
+userEmail = "vasudev@gmail.com";
+userAdd = "Raj";
+
+console.log(userId);
+console.table([userId, userName, userEmail, userAdd, accountState]);
