19d8a04d9c2f9f2a89262484874961fdd3abc802
Vasudevsuthar <133574340+Vasudevsuthar@users.noreply.github.com>
2024-04-05 06:45:32 +0000

    conversion

---
+++ .devcontainer/conversion.js
@@ -0,0 +1,36 @@
+//number conversion//
+let a = undefined;
+let valueInNumber = Number(a);
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+//OutPut//
+// 123 => 123;
+// 123abc => NaN
+// true => 1; false => 
+// undefined => NaN
+
+// boolean conversion//
+let b = undefined;
+let valueInBoolean = Boolean(b);
+console.log(valueInBoolean);
+console.log(typeof valueInBoolean);
+
+//OutPut//
+//"vasu" => true
+// "" => flase
+// 1 => true
+// 0 => false
+// undefined => false
+
+// string conversion//
+let c = undefined;
+let valueInString = String(c);
+console.log(valueInString);
+console.log(typeof valueInString);
+
+//OutPut//
+// 1 => string
+// true => string; false => string
+// undefined => string
+
